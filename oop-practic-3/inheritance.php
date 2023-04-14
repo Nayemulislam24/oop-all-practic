e39f90ce801946753b39ff814a4add772c39f297
MD Nayem <nayemtp@gmail.com>
2023-04-14 12:08:24 +0600

    Re-practic

--- oop-practic-3/inheritance.php
+++ oop-practic-3/inheritance.php
@@ -1,45 +1,42 @@
 <?php
-class departmentinfo{
-    public $dep_chairman;
-    public $dep_name;
-    public $dep_classRoom;
-    public $dep_student;
-
-public function __construct($chair,$dept, $room, $stdNumber){
-
-$this->dep_chairman=$chair;
-$this->dep_name=$dept;
-$this->dep_classRoom=$room;
-$this->dep_student=$stdNumber;
-echo "this output constructor";
-echo "<br>";
-echo $this->dep_chairman;
-echo "<br>";
-echo $this->dep_name;
-echo "<br>";
-echo "this output constructor end";
-echo "<br>";
+class DepartmentInfo
+{
+    public $departmentName;
+    public $chairman;
+    public $classRoom;
+    public $numberOfStudents;
+    public function __construct($dept, $chair, $room, $stdNumber)
+    {
+        $this->departmentName = $dept;
+        $this->chairman = $chair;
+        $this->classRoom = $room;
+        $this->numberOfStudents = $stdNumber;
     }
+    public function departmentActivity()
+    {
+        echo "Deartment of " . $this->departmentName . " arrange a tour in every year" . "<br/>";
+    }
+}
+// class StatisticsDept extends DepartmentInfo
+// {
+//     public $labsNumber;
+// }
 
-public function dep_EEE(){
-    echo "department  chairman name"   .$this->dep_chairman   ."departman of EEE";
-    echo "<br>";
+class CseDept extends DepartmentInfo
+{
+    public $labsNumber;
+    public function programmingActivity()
+    {
+        echo "Department of " . $this->departmentName . " should arrange programming contest like other university" . "<br/>";
     }
 }
 
-class dep_CSE extends departmentinfo{
-    public $lab_number;
-    public function labNumber(){
-        return "cse lab_no-01";
-    }
+// $statistics = new StatisticsDept("Statistics", "Dr. Rowshan Jahan", 6, 300);
+// echo "Chairman - " . $statistics->chairman . " * Students - " . $statistics->numberOfStudents . "<br/>";
+// $statistics->departmentActivity();
 
 
-}
-
-
-
-$test = new dep_CSE("Dr.ATM shamsush zaman","CSE",502,20);
-$test->dep_EEE();
-echo $test->labNumber();
-
-?>
+$cse = new CseDept("Computer Science and Engineering", "Dr.Jugal Krishna Das", 5, 300);
+// echo "Chairman - " . $cse->chairman . " * Students - " . $cse->numberOfStudents . "<br/>";
+$cse->departmentActivity();
+$cse->programmingActivity();
